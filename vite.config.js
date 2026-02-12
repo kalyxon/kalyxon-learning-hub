fe90a0fec7622991ecacfa3dcb33cad52cbf22b7
kalyxon <k4lyx0n@gmail.com>
2026-02-12 23:24:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kalyxon/',
+  base: '/kalyxon/', // Replace with "/" 
   server: {
     port: 3000,
     open: true,
