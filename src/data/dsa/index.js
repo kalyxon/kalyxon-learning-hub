f5e488d9c642d8d4e3f3503dcb65c3ecfebffed7
kalyxon <k4lyx0n@gmail.com>
2026-02-23 22:51:33 +0530

    Create index.js

---
+++ src/data/dsa/index.js
@@ -0,0 +1,14 @@
+import { arraysTutorial } from './arrays';
+import { linkedListTutorial } from './linked-list';
+import { recursionTutorial } from './recursion';
+import { binarySearchTutorial } from './binary-search';
+
+export const dsaTutorials = [
+  arraysTutorial,
+  linkedListTutorial,
+  recursionTutorial,
+  binarySearchTutorial
+];
+
+// Optional: Log to verify
+console.log(`📚 DSA tutorials loaded: ${dsaTutorials.length}`);
